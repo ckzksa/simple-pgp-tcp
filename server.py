f38d920fd31c6a5ead9df6a8d13970ab8df3a421
ckzksa <kevin.cerkiewicz@gmail.com>
2023-08-10 19:30:09 +0200

    error username displayed

--- server.py
+++ server.py
@@ -9,13 +9,13 @@ PORT = 4321
 server_socket = None
 clients = {}
 
-def broadcast(sender, message):
+def broadcast(sender_address, sender_username, message):
     for address, (conn, username) in clients.items():
-        if sender == address:
+        if sender_address == address:
             continue
         
         try:
-            conn.send(f"<{username}> {message}".encode('utf-8'))
+            conn.send(f"<{sender_username}> {message}".encode('utf-8'))
         except Exception as e:
             print(e)
             print(f"Error sending message to {username}")
@@ -29,7 +29,7 @@ def thread_client(conn, address):
     while True:
         try:
             message = conn.recv(1024).decode('utf-8')
-            broadcast(address, message)
+            broadcast(address, username, message)
         except Exception as e:
             print(e)
             conn.close()
