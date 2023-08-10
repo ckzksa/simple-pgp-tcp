59feefdd8ffed9f1197ace500a72d534c4219dbe
ckzksa <kevin.cerkiewicz@gmail.com>
2023-08-10 18:12:10 +0200

    clear & add encode in server

--- client.py
+++ client.py
@@ -1,59 +1,39 @@
 import socket
 import threading
 
-SERVER = "127.0.0.1"
+SERVER = "192.168.1.9"
 PORT = 4321
-MSG_MAX_SIZE = 1024
-
-def init_connection():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("press enter to connect to the server")
-    input()
-    try:
-        sock.connect((SERVER, PORT))
-    except Exception as e:
-        print(e)
-    print(f"connecting to server {SERVER} : {PORT}")
-    return sock
-
 
 def start_send(sock):
-    talk = True
-    while talk:
-        message = input()
-        if message == None or len(message) < 1 :
-            print ("wrong message")
-        else:
-            try:
-                sock.send(message.encode("utf-8"))
-            except Exception as e:
-                print(e)
-                talk=False
     try:
-        sock.close()
+        while True:
+            message = input()
+            if message == None or len(message) < 1 :
+                pass
+            else:
+                sock.send(message.encode("utf-8"))
     except Exception as e:
-            print(e)
+        print(e)
+        sock.close()
 
 
 def start_receive(sock):
-    talk = True
-    while talk:
-        try:
-            message = sock.recv(MSG_MAX_SIZE).decode("utf-8")
-            print(f"{message}")
-        except Exception as e:
-            print(e)
-            talk=False
     try:
-        sock.close()
+        while True:
+            message = sock.recv(1024).decode("utf-8")
+            print(f"{message}")
     except Exception as e:
-            print(e)
+        print(e)
+        sock.close()
 
 
 if __name__ =="__main__":
-    sock = init_connection()
-    thread_send = threading.Thread(target=start_send, args = (sock))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((SERVER, PORT))
+    print(f"Connected to server {SERVER} : {PORT}")
+    
+    thread_send = threading.Thread(target=start_send, args = (sock,))
     thread_send.start()
     
-    thread_rcv = threading.Thread(target=start_receive, args = (sock))
+    thread_rcv = threading.Thread(target=start_receive, args = (sock,))
     thread_rcv.start()
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import threading
 import socket
 import time
+import sys
 
 HOST = "0.0.0.0"
 PORT = 4321
@@ -13,29 +14,30 @@ def thread_client(conn, address):
             message = conn.recv(1024).decode('utf-8')
             print(message)
             time.sleep(3)
-            conn.send("server a recu")
-        except:
+            conn.send("<server> recu".encode('utf-8'))
+        except Exception as e:
+            print(e)
             conn.close()
             del clients[address]
             break
 
 def start_server():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        s.bind((HOST, PORT))
-        s.listen()
-        print(f"Listen on {HOST}:{PORT}")
-    except Exception as e:
-        print(e)
+    s.settimeout(4)
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Listen on {HOST}:{PORT}")
 
     while True:
-        conn, address = s.accept()
-        
-        print(f"New client {address}")
-        clients[address] = conn
-        
-        thread = threading.Thread(target=thread_client, args=(conn, address))
-        thread.start()
+        try:
+            conn, address = s.accept()
+            clients[address] = conn
+            thread = threading.Thread(target=thread_client, args=(conn, address))
+            thread.start()
+            print(f"New client {address}")
+        except TimeoutError:
+            print("timeout")
+            pass
 
 if __name__ == "__main__":
     start_server()
