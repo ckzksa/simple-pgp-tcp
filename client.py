764b176c1b7a8600c4f10dd2276cef33eec81e5d
ckzksa <kevin.cerkiewicz@gmail.com>
2023-08-16 16:38:42 +0200

    ugly commit
    add security
    add handshake
    move send/receive functions in client/server file

--- client.py
+++ client.py
@@ -3,7 +3,11 @@ import threading
 import signal
 import logging
 import rich
-import message as msg
+
+from rich.prompt import Prompt
+from security import Security
+
+#TODO utiliser selects
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
@@ -17,21 +21,26 @@ log.addHandler(file_handler)
 SERVER = "127.0.0.1"
 PORT = 4321
 
+secure = Security(save_keys=False)
 client_socket = None
+session_key = None
+nonce = None
 username = ""
 
 def client_send(client_socket):
     try:
         while True:
-            message = ""
-            while not message:
+            payload = ""
+            while not payload:
                 rich.print(f"<[bold violet]{username}[/bold violet]> ", end='')
-                message = input()
+                payload = input()
                 
-            if message == None or len(message) < 1 :
+            if not payload:
                 pass
             else:
-                msg.send(client_socket, message)
+                payload, _ = secure.encrypt_aes(session_key, nonce, payload.encode("utf-8"))
+                send(client_socket, payload)
+                
     except EOFError:
         pass
     except Exception as e:
@@ -41,8 +50,9 @@ def client_send(client_socket):
 def client_receive(client_socket):
     try:
         while True:
-            message = msg.receive(client_socket)
-            print(f"{message}")
+            payload = receive(client_socket)
+            payload = secure.decrypt_aes(key=session_key, nonce=nonce, ciphertext=payload)
+            print(f"{payload.decode('utf-8')}")
     except ConnectionResetError:
         print("Connection reset by host.")
         log.info(f"Connection reset by host.")
@@ -50,22 +60,60 @@ def client_receive(client_socket):
         log.info(f"Connection aborted.")
     except Exception as e:
         log.error(e)
-        
+
+def send(sock, payload):
+    sock.sendall(payload + b"\n\n")
+
+def receive(sock):
+    payload = b""
+    
+    while not payload.endswith(b"\n\n"):
+        data = sock.recv(1024)
+        if not data:
+            return None
+        payload = payload + data
+    
+    return payload[:-2]
+
+# Handle connection
+def handshake(sock):#TODO clear le code
+    global session_key
+    global nonce
+    
+    # receive server public key
+    # data = receive(sock)
+    # server_pubkey = secure.import_key(data, passphrase=b"charlie")
+    
+    # send public key
+    pubkey = secure.export_key()
+    send(sock, pubkey)
+    
+    # receive session key
+    data = receive(sock)
+    data = secure.decrypt_rsa(data, key=secure.private_key)
+    session_key = data[:16]
+    nonce = data[16:]
+    
+    # send username
+    data, _ = secure.encrypt_aes(key=session_key, nonce=nonce, payload=username.encode("utf-8"))
+    send(sock, data)
+    
+    return client
 
 def client():
     global username
     global client_socket
     
+    while not username:
+        username = Prompt.ask("[bold green]Username[/bold green]")
+    
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((SERVER, PORT))
     
+    handshake(client_socket)
+    
     log.debug(f"Connected to server {SERVER} : {PORT}.")
     print(f"Connected to server {SERVER} : {PORT}.")
-    
-    while not username:
-        print("Name: ", end='')
-        username = input()
-    msg.send(client_socket, username)
     
     send_thread = threading.Thread(target=client_send, args = (client_socket,), daemon = True)
     receive_thread = threading.Thread(target=client_receive, args = (client_socket,), daemon = True)
@@ -78,8 +126,9 @@ def client():
 def sigint_handler(sig, frame):
     log.info("SIGINT received, closing the socket and exiting.")
     global client_socket
-    client_socket.close()
+    if client_socket:
+        client_socket.close()
 signal.signal(signal.SIGINT, sigint_handler)
     
 if __name__ =="__main__":
-    client()
+    client()#TODO passer en classe
--- server.py
+++ server.py
@@ -3,9 +3,11 @@ import socket
 import sys
 import signal
 import logging
-import message as msg
 
+from security import Security
 from logging.handlers import TimedRotatingFileHandler
+
+#TODO utiliser select
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
@@ -27,54 +29,109 @@ log.addHandler(file_handler)
 HOST = "0.0.0.0"
 PORT = 4321
 
+security = Security(save_keys=True)
 server_socket = None
 clients = {}
 
+class HandshakeError(Exception):
+    pass
+
+class Client():
+    def __init__(self, sock, address, username, pubkey, session_key, nonce):
+        self.sock = sock
+        self.address = address
+        self.username = username
+        self.pubkey = pubkey
+        self.session_key = session_key
+        self.nonce = nonce
+
 # broadcast a message to every clients except sender
-def broadcast(sender_address, sender_username, message):
-    for address, (conn, username) in clients.items():
+def broadcast(sender, payload):
+    for _, client in clients.items():
         # don't send message to sender
-        if sender_address == address:
+        if sender and sender.address == client.address:
             continue
         
         try:
-            msg.send(conn, f"<{sender_username}> {message}")
+            enc_payload, _ = security.encrypt_aes(client.session_key,
+                                                  client.nonce,
+                                                  f"<{sender.username if sender else 'SERVER'}> {payload}".encode("utf-8"))
+            send(client.sock, enc_payload)
         except Exception as e:
-            log.error(f"Error sending message to {username} {address}.")
+            log.error(f"Error sending message to {client.username} {client.address}. {e}")
 
-# Handle login
-def login(conn, address):
-    username = msg.receive(conn)
-    
-    clients[address] = (conn, username)
-    broadcast(None, "SERVER", f"{username} joined the chat")
-    
-    return username
-
-# Handle the client connection
+# Handle the client
 def client_thread(conn, address):
-    username = login(conn, address)
-    
-    # communicate
     try:
+        client = handshake(conn, address)
         while True:
-            message = msg.receive(conn)
-            if not message:
+            payload = receive(conn)
+            if not payload:
                 break
-            log.debug(f"New message from {username}: {message}.")
-            broadcast(address, username, message)
+            
+            payload = security.decrypt_aes(key=client.session_key, nonce=client.nonce, ciphertext=payload)
+            payload = payload.decode('utf-8')
+            log.debug(f"New message from {client.username}: {payload}.")
+            broadcast(client, payload)
     except ConnectionResetError:
         log.info(f"Connection reset {address}.")
     except ConnectionAbortedError:
         pass
+    except HandshakeError:
+        log.error(f"Error in handshake {address}.")
     except Exception as e:
         log.error(e)
     finally:
         conn.close()
         del clients[address]
         log.info(f"Socket closed {address}.")
-        broadcast(None, "SERVER", f"{username} left the chat")
-        
+        broadcast(None, f"{client.username} left the chat")
+
+# send the whole message and end it with a double newline
+def send(sock, payload):
+    sock.sendall(payload + b"\n\n")
+
+# retrieve data until a double newline
+def receive(sock):
+    payload = b""
+    
+    while not payload.endswith(b"\n\n"): #TODO changer ca, risque faible d'erreur
+        data = sock.recv(1024)
+        if not data:
+            return None
+        payload = payload + data
+    
+    return payload[:-2]
+
+# Handle connection
+def handshake(sock, address):#TODO clear le code
+    global clients
+    
+    # enc_pubkey = secure.export_key(passphrase=b"charlie")
+    # send(sock, enc_pubkey)
+    
+    # receive client public key
+    data = receive(sock)
+    if not data:
+        raise HandshakeError
+    pubkey = security.import_key(data)
+    
+    # send session key
+    session_key, nonce = security.generate_aes_key()
+    data = security.encrypt_rsa(session_key + nonce, key=pubkey)
+    send(sock, data)
+    
+    #receive and broadcast username
+    data = receive(sock)
+    if not data:
+        raise HandshakeError
+    username = security.decrypt_aes(key=session_key, nonce=nonce, ciphertext=data)
+    
+    client = Client(sock, address, username.decode('utf-8'), pubkey, session_key, nonce)
+    clients[address] = client
+    broadcast(None, f"{client.username} joined the chat")
+    
+    return client
 
 # start server and client threads
 def server():
@@ -99,11 +156,12 @@ def server():
 def sigint_handler(sig, frame):
     log.info("SIGINT received, closing the socket and exiting.")
     global server_socket
-    server_socket.close()
-    for _, (conn, _) in clients.items():
-        conn.close()
+    if server_socket:
+        server_socket.close()
+    for _, client in clients.items():
+        client.sock.close()
     sys.exit(0)
 signal.signal(signal.SIGINT, sigint_handler)
 
 if __name__ == "__main__":
-    server()
+    server()#TODO passer en classe
